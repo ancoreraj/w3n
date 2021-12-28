6847235cdb5991ddd8f278e8a6585847c2e4af52
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2021-12-28 13:22:58 +0530

    UI done

--- components/Index.js
+++ components/Index.js
@@ -1,52 +1,50 @@
 import React from 'react'
 import { View, Text, StyleSheet, StatusBar } from 'react-native'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Entypo, FontAwesome } from '@expo/vector-icons';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 import News from '../screens/News/News';
 import Discover from '../screens/Discover/Discover';
 
 const Tab = createMaterialTopTabNavigator();
 
 const Index = () => {
+
     return (
         <>
             <Tab.Navigator
                 tabBarOptions={{
                     style: {
-                        backgroundColor: "#262250",
+                        backgroundColor: "#fff",
                         paddingTop: StatusBar.currentHeight,
                     },
                     indicatorStyle: {
-                        backgroundColor: "white"
+                        backgroundColor: "#262250"
                     }
                 }}
-
             >
                 <Tab.Screen
                     options={{
                         title: ({ color, focused }) => (
                             <View style={styles.tabContainer}>
-                                <Entypo name="home" size={26} color={focused ? 'white' : '#B1AFCD'} />
+                                <Feather name="home" size={27} color={focused ? '#312F62' : '#B1AFCD'} />
                             </View>
-
-
                         ),
                     }}
-                    name="News" component={News} />
+                    name="News" component={Discover} />
                 <Tab.Screen
                     options={{
                         title: ({ color, focused }) => (
-                            <FontAwesome name="newspaper-o" size={24} color={focused ? 'white' : '#B1AFCD'} />
+                            <View style={styles.tabContainer}>
+                                <FontAwesome name="newspaper-o" size={26} color={focused ? '#312F62' : '#B1AFCD'} />
+                            </View>
                         ),
-                    }} name="Discover" component={Discover} />
+                    }} name="Discover" component={News} />
             </Tab.Navigator>
         </>
     )
 }
 
 const styles = StyleSheet.create({
-    tabContainer: {
-        padding: 5
-    }
+
 });
 export default Index
--- screens/Discover/Discover.js
+++ screens/Discover/Discover.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Discover = () => {
     return (
-        <View>
-            <Text>This is discover tab</Text>
+        <View style={styles.slide1}>
+            <Text style={styles.text}>Hello Swiper</Text>
         </View>
     )
 }
 
 export default Discover
+
+const styles = StyleSheet.create({
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#312F62'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontFamily: 'Inter-Black'
+    }
+});
